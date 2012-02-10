1c69403427abeb8887c9186aa6637a13eafdd19f
Jaromir Tomek <jaromir.tomek@gmail.com>
2012-02-10 10:53:17 +0100

    creatures removed from the book

--- db/migrations/0001_table_creatures.sql
+++
@@ -1,9 +0,0 @@
--- just a sample table for demonstrating ATK14 functions (see CreaturesController)
--- you can `DROP TALE creatures` in your future migration
-CREATE SEQUENCE seq_creatures;
-CREATE TABLE creatures(
-	id INT NOT NULL PRIMARY KEY DEFAULT NEXTVAL('seq_creatures'),
-	name VARCHAR(255),
-	description TEXT,
-	image_url VARCHAR(255)
-);
--- db/migrations/0002_content_for_creatures.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO creatures (name,description,image_url) VALUES('ATK14 Birdie','A sort of attractive creature.','http://www.atk14.net/public/images/atk14.gif');
-INSERT INTO creatures (name,description) VALUES('An ordinary creature','Normal creature. No picture is needed.');
